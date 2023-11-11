dd0d35414217ecf0893feb30611f9f4e7fb0b4d6
tomoikey <55743826+tomoikey@users.noreply.github.com>
2023-11-12 07:26:43 +0900

    implement RuleBinder

--- src/refined.rs
+++ src/refined.rs
@@ -33,27 +33,6 @@ where
             _rule: Default::default(),
         })
     }
-
-    pub fn add_rule<NewRule>(self, rule: &NewRule) -> Result<Refined<NewRule, T>>
-    where
-        NewRule: Rule<TARGET = T>,
-    {
-        Refined::new(self.value, rule)
-    }
-
-    pub fn add_rules<NewRule>(self, rules: &Vec<NewRule>) -> Result<Self>
-    where
-        NewRule: Rule<TARGET = T>,
-    {
-        let mut result = self.value;
-        for rule in rules {
-            result = NewRule::validate(rule, result)?;
-        }
-        Ok(Refined {
-            value: result,
-            _rule: Default::default(),
-        })
-    }
 }
 
 impl<RULE, ITERATOR, T> Refined<RULE, ITERATOR>
@@ -98,7 +77,6 @@ mod test {
     use crate::error::Result;
     use crate::refined::Refined;
     use crate::rule::NonEmptyStringRule;
-    use crate::AlphabetRule;
     use std::collections::HashSet;
 
     #[test]
@@ -162,22 +140,6 @@ mod test {
     fn test_refined_display() -> Result<()> {
         let non_empty_string = Refined::new("Hello".to_string(), &NonEmptyStringRule)?;
         assert_eq!(format!("{}", non_empty_string), "Hello");
-        Ok(())
-    }
-
-    #[test]
-    fn test_refined_add_rule_ok() -> Result<()> {
-        let non_empty_string =
-            Refined::new("Hello".to_string(), &NonEmptyStringRule)?.add_rule(&AlphabetRule)?;
-        assert_eq!(non_empty_string.value, "Hello");
-        Ok(())
-    }
-
-    #[test]
-    fn test_refined_add_rule_err() -> Result<()> {
-        let non_empty_string =
-            Refined::new("Hello1".to_string(), &NonEmptyStringRule)?.add_rule(&AlphabetRule);
-        assert!(non_empty_string.is_err());
         Ok(())
     }
 }
--- src/rule.rs
+++ src/rule.rs
@@ -2,10 +2,64 @@ mod number;
 mod string;
 
 use crate::error::Result;
+use std::marker::PhantomData;
+use std::ops::Deref;
+
 pub use string::*;
 
 /// This is a `trait` that specifies the conditions a type `T` should satisfy
 pub trait Rule {
     type TARGET;
     fn validate(&self, target: Self::TARGET) -> Result<Self::TARGET>;
+}
+
+pub struct RuleBinder<'a, T, Rule1, Rule2> {
+    bounden_rule: Box<dyn Fn(T) -> Result<T> + 'a>,
+    _rule1: PhantomData<Rule1>,
+    _rule2: PhantomData<Rule2>,
+}
+
+impl<'a, T, Rule1, Rule2> RuleBinder<'a, T, Rule1, Rule2>
+where
+    Rule1: Rule<TARGET = T> + 'a,
+    Rule2: Rule<TARGET = T> + 'a,
+{
+    pub fn bind(rule1: Rule1, rule2: Rule2) -> Self {
+        let bounded_rule = move |t: T| rule1.validate(t).and_then(|t| rule2.validate(t));
+        Self {
+            bounden_rule: Box::new(bounded_rule),
+            _rule1: Default::default(),
+            _rule2: Default::default(),
+        }
+    }
+}
+
+impl<T, Rule1, Rule2> Rule for RuleBinder<'_, T, Rule1, Rule2> {
+    type TARGET = T;
+
+    fn validate(&self, target: Self::TARGET) -> Result<Self::TARGET> {
+        self.bounden_rule.deref()(target)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::error::Result;
+    use crate::{AlphabetRule, NonEmptyStringRule, Rule, RuleBinder};
+
+    #[test]
+    fn test_rule_binder_ok() -> Result<()> {
+        let non_empty_alphabet_rule = RuleBinder::bind(NonEmptyStringRule, AlphabetRule);
+        let actual = non_empty_alphabet_rule.validate("Hello".to_string())?;
+        assert_eq!(actual, "Hello");
+        Ok(())
+    }
+
+    #[test]
+    fn test_rule_binder_err() -> Result<()> {
+        let non_empty_alphabet_rule = RuleBinder::bind(NonEmptyStringRule, AlphabetRule);
+        let actual = non_empty_alphabet_rule.validate("Hello1".to_string());
+        assert!(actual.is_err());
+        Ok(())
+    }
 }
