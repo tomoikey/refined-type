c9be7d7e0b110a868fde46c0235d03ee458ba6bb
tomoikey <55743826+tomoikey@users.noreply.github.com>
2023-11-12 04:31:28 +0900

    add supports for collections

--- src/refined.rs
+++ src/refined.rs
@@ -1,6 +1,8 @@
 use crate::rule::Rule;
 use anyhow::Result;
+use std::collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList, VecDeque};
 use std::fmt::{Display, Formatter};
+use std::hash::Hash;
 use std::marker::PhantomData;
 use std::ops::Deref;
 
@@ -29,6 +31,137 @@ where
         let mut result = Vec::new();
         for i in value {
             result.push(RULE::validate(i)?)
+        }
+        Ok(Self {
+            value: result,
+            _phantom_data: Default::default(),
+        })
+    }
+}
+
+impl<RULE, T> TryFrom<VecDeque<T>> for Refined<RULE, VecDeque<T>>
+where
+    RULE: Rule<TARGET = T>,
+{
+    type Error = anyhow::Error;
+
+    fn try_from(value: VecDeque<T>) -> std::result::Result<Self, Self::Error> {
+        let mut result = VecDeque::new();
+        for i in value {
+            result.push_back(RULE::validate(i)?)
+        }
+        Ok(Self {
+            value: result,
+            _phantom_data: Default::default(),
+        })
+    }
+}
+
+impl<RULE, T> TryFrom<LinkedList<T>> for Refined<RULE, LinkedList<T>>
+where
+    RULE: Rule<TARGET = T>,
+{
+    type Error = anyhow::Error;
+
+    fn try_from(value: LinkedList<T>) -> std::result::Result<Self, Self::Error> {
+        let mut result = LinkedList::new();
+        for i in value {
+            result.push_back(RULE::validate(i)?)
+        }
+        Ok(Self {
+            value: result,
+            _phantom_data: Default::default(),
+        })
+    }
+}
+
+impl<RULE, K, V> TryFrom<BTreeMap<K, V>> for Refined<RULE, BTreeMap<K, V>>
+where
+    RULE: Rule<TARGET = V>,
+    K: Ord,
+{
+    type Error = anyhow::Error;
+
+    fn try_from(value: BTreeMap<K, V>) -> std::result::Result<Self, Self::Error> {
+        let mut result = BTreeMap::new();
+        for (k, v) in value {
+            result.insert(k, RULE::validate(v)?);
+        }
+        Ok(Self {
+            value: result,
+            _phantom_data: Default::default(),
+        })
+    }
+}
+
+impl<RULE, K, V> TryFrom<HashMap<K, V>> for Refined<RULE, HashMap<K, V>>
+where
+    RULE: Rule<TARGET = V>,
+    K: Eq + Hash,
+{
+    type Error = anyhow::Error;
+
+    fn try_from(value: HashMap<K, V>) -> Result<Self, Self::Error> {
+        let mut result = HashMap::new();
+        for (k, v) in value {
+            result.insert(k, RULE::validate(v)?);
+        }
+        Ok(Self {
+            value: result,
+            _phantom_data: Default::default(),
+        })
+    }
+}
+
+impl<RULE, T> TryFrom<HashSet<T>> for Refined<RULE, HashSet<T>>
+where
+    RULE: Rule<TARGET = T>,
+    T: Eq + Hash,
+{
+    type Error = anyhow::Error;
+
+    fn try_from(value: HashSet<T>) -> Result<Self, Self::Error> {
+        let mut result = HashSet::new();
+        for i in value {
+            result.insert(RULE::validate(i)?);
+        }
+        Ok(Self {
+            value: result,
+            _phantom_data: Default::default(),
+        })
+    }
+}
+
+impl<RULE, T> TryFrom<BTreeSet<T>> for Refined<RULE, BTreeSet<T>>
+where
+    RULE: Rule<TARGET = T>,
+    T: Ord,
+{
+    type Error = anyhow::Error;
+
+    fn try_from(value: BTreeSet<T>) -> Result<Self, Self::Error> {
+        let mut result = BTreeSet::new();
+        for i in value {
+            result.insert(RULE::validate(i)?);
+        }
+        Ok(Self {
+            value: result,
+            _phantom_data: Default::default(),
+        })
+    }
+}
+
+impl<RULE, T> TryFrom<BinaryHeap<T>> for Refined<RULE, BinaryHeap<T>>
+where
+    RULE: Rule<TARGET = T>,
+    T: Ord,
+{
+    type Error = anyhow::Error;
+
+    fn try_from(value: BinaryHeap<T>) -> std::result::Result<Self, Self::Error> {
+        let mut result = BinaryHeap::new();
+        for i in value {
+            result.push(RULE::validate(i)?);
         }
         Ok(Self {
             value: result,
