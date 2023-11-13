5dc020ee80c09cc83aadef711a5fcf1ef35951c3
tomoikey <55743826+tomoikey@users.noreply.github.com>
2023-11-14 04:10:46 +0900

    add a doc comment for or

--- src/rule/composer/or.rs
+++ src/rule/composer/or.rs
@@ -6,6 +6,19 @@ use std::ops::Deref;
 /// A binder that combines two rules to generate a new single `Rule`
 /// # Example
 /// ```rust
+/// use refined_type::rule::{LessI8Rule, MoreI8Rule, Rule};
+/// use refined_type::rule::composer::Or;
+///
+/// let less_than_1 = LessI8Rule::new(1);
+/// let more_than_5 = MoreI8Rule::new(5);
+/// let rule = Or::new(less_than_1, more_than_5);
+///
+/// assert!(rule.validate(0).is_ok());
+/// assert!(rule.validate(1).is_ok());
+/// assert!(rule.validate(2).is_err());
+/// assert!(rule.validate(4).is_err());
+/// assert!(rule.validate(5).is_ok());
+/// assert!(rule.validate(6).is_ok());
 /// ```
 pub struct Or<'a, T, RULE1, RULE2> {
     bounden_rule: Box<dyn Fn(T) -> Result<T, Error<T>> + 'a>,
