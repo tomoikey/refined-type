a11d81e9e11940f5af2a6a5b00253d8da9dd805b
tomoikey <55743826+tomoikey@users.noreply.github.com>
2024-02-21 12:10:56 +0900

    ...

--- src/rule/non_empty/non_empty_vec.rs
+++ src/rule/non_empty/non_empty_vec.rs
@@ -1,23 +1,8 @@
-use crate::rule::closed_algebraic::ClosedAlgebraic;
 use crate::rule::NonEmpty;
 use crate::Refined;
-use std::fmt::Debug;
 
 pub type NonEmptyVec<T> = Refined<NonEmptyVecRule<T>>;
 pub type NonEmptyVecRule<T> = NonEmpty<Vec<T>>;
-
-/// # Math Theory
-/// NonEmptyVec + NonEmptyVec = NonEmptyVec
-impl<T> ClosedAlgebraic for NonEmptyVec<T>
-where
-    T: Debug,
-{
-    fn plus(self, that: NonEmptyVec<T>) -> NonEmptyVec<T> {
-        let mut result = self.into_value();
-        result.append(&mut that.into_value());
-        Refined::new(result).unwrap()
-    }
-}
 
 #[cfg(test)]
 mod test {
