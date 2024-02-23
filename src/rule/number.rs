4c80853108e122bc61eef295d0d523317b028094
tomoikey <55743826+tomoikey@users.noreply.github.com>
2024-02-23 20:40:21 +0900

    impl Equal

--- src/rule/number.rs
+++ src/rule/number.rs
@@ -1,3 +1,4 @@
+mod equal;
 mod even;
 mod greater;
 mod less;
--- src/rule/number/equal.rs
+++ src/rule/number/equal.rs
@@ -1,18 +1,18 @@
 #[macro_export]
-macro_rules! greater_rule {
+macro_rules! equal_rule {
     (($e: literal, $t: ty)) => {
         paste::item! {
-            pub type [<Greater $e $t>] = $crate::Refined<[<GreaterRule $e $t>]>;
-            pub struct [<GreaterRule $e $t>];
+            pub type [<Equal $e $t>] = $crate::Refined<[<EqualRule $e $t>]>;
+            pub struct [<EqualRule $e $t>];
 
-            impl $crate::rule::Rule for [<GreaterRule $e $t>] {
+            impl $crate::rule::Rule for [<EqualRule $e $t>] {
                 type Item = $t;
 
                 fn validate(target: Self::Item) -> Result<Self::Item, $crate::result::Error<Self::Item>> {
-                    if target > $e {
+                    if target == $e {
                         Ok(target)
                     } else {
-                        Err($crate::result::Error::new(format!("{} is not greater than {}", target, $e), target))
+                        Err($crate::result::Error::new(format!("{} does not equal {}", target, $e), target))
                     }
                 }
             }
@@ -26,17 +26,17 @@ macro_rules! greater_rule {
 
 #[cfg(test)]
 mod test {
-    greater_rule!((50, i8));
+    equal_rule!((50, i8));
 
     #[test]
-    fn test_greater_than_50i8_ok() {
-        let less_result = Greater50i8::new(100);
+    fn test_equal_than_50i8_ok() {
+        let less_result = Equal50i8::new(50);
         assert!(less_result.is_ok());
     }
 
     #[test]
-    fn test_greater_than_50i8_err() {
-        let less_result = Greater50i8::new(50);
+    fn test_equal_than_50i8_err() {
+        let less_result = Equal50i8::new(100);
         assert!(less_result.is_err());
     }
 }
