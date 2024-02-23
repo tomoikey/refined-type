761cab3aaad9b8a0006258d80874de8018b605c1
tomoikey <55743826+tomoikey@users.noreply.github.com>
2024-02-23 16:16:45 +0900

    vec dequeのpushとかを追加

--- src/rule/non_empty/non_empty_vec_deque.rs
+++ src/rule/non_empty/non_empty_vec_deque.rs
@@ -23,6 +23,34 @@ where
             .ok()
             .expect("This error is always unreachable")
     }
+
+    pub fn get(&self, index: usize) -> Option<&T> {
+        self.value().get(index)
+    }
+
+    pub fn len(&self) -> usize {
+        self.value().len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        false
+    }
+
+    pub fn push_front(self, value: T) -> Self {
+        let mut result = self.into_value();
+        result.push_front(value);
+        Refined::new(result)
+            .ok()
+            .expect("This error is always unreachable")
+    }
+
+    pub fn push_back(self, value: T) -> Self {
+        let mut result = self.into_value();
+        result.push_back(value);
+        Refined::new(result)
+            .ok()
+            .expect("This error is always unreachable")
+    }
 }
 
 impl<T> Add for NonEmptyVecDeque<T> {
@@ -34,5 +62,50 @@ impl<T> Add for NonEmptyVecDeque<T> {
         Refined::new(result)
             .ok()
             .expect("This error is always unreachable")
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::rule::non_empty::non_empty_vec_deque::NonEmptyVecDeque;
+    use std::collections::VecDeque;
+
+    #[test]
+    fn test_vec_deque_push() -> anyhow::Result<()> {
+        let mut deque = VecDeque::new();
+        deque.push_front(1);
+        let deque = NonEmptyVecDeque::new(deque)?.push_front(2).push_back(3);
+        assert_eq!(
+            deque.into_value().into_iter().collect::<Vec<_>>(),
+            vec![2, 1, 3]
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_get() -> anyhow::Result<()> {
+        let mut deque = VecDeque::new();
+        deque.push_front(1);
+        let deque = NonEmptyVecDeque::new(deque)?;
+        assert_eq!(deque.get(0), Some(&1));
+        Ok(())
+    }
+
+    #[test]
+    fn test_len() -> anyhow::Result<()> {
+        let mut deque = VecDeque::new();
+        deque.push_front(1);
+        let deque = NonEmptyVecDeque::new(deque)?;
+        assert_eq!(deque.len(), 1);
+        Ok(())
+    }
+
+    #[test]
+    fn test_is_empty() -> anyhow::Result<()> {
+        let mut deque = VecDeque::new();
+        deque.push_front(1);
+        let deque = NonEmptyVecDeque::new(deque)?;
+        assert!(!deque.is_empty());
+        Ok(())
     }
 }
