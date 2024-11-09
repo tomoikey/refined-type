1f7378ba0e387fa88738182d48afcdfb15727e04
tomoikey <55743826+tomoikey@users.noreply.github.com>
2024-11-10 01:48:23 +0900

    remove lifetime parameter

--- src/rule/collection/init.rs
+++ src/rule/collection/init.rs
@@ -3,29 +3,29 @@ use crate::Refined;
 use std::collections::VecDeque;
 
 /// A type that holds a value satisfying the `InitRule`
-pub type Init<'a, RULE, ITERABLE> = Refined<InitRule<'a, RULE, ITERABLE>>;
+pub type Init<'a, RULE, ITERABLE> = Refined<InitRule<RULE, ITERABLE>>;
 
 /// A type that holds a Vec value satisfying the `InitRule`
-pub type InitVec<'a, RULE> = Refined<InitVecRule<'a, RULE>>;
+pub type InitVec<RULE> = Refined<InitVecRule<RULE>>;
 
 /// A type that holds a VecDeque value satisfying the `InitRule`
-pub type InitVecDeque<'a, RULE> = Refined<InitVecDequeRule<'a, RULE>>;
+pub type InitVecDeque<RULE> = Refined<InitVecDequeRule<RULE>>;
 
 /// A type that holds a String value satisfying the `InitRule`
-pub type InitString<'a, RULE> = Refined<InitStringRule<'a, RULE>>;
+pub type InitString<'a, RULE> = Refined<InitStringRule<RULE>>;
 
 /// Rule that applies to the initialization of a collection
-pub type InitRule<'a, RULE, ITERABLE> =
-    ReverseRule<'a, SkipRule<RULE, ITERABLE, SkipFirst<<ITERABLE as Iterable<'a>>::Item>>>;
+pub type InitRule<RULE, ITERABLE> =
+    ReverseRule<SkipRule<RULE, ITERABLE, SkipFirst<<ITERABLE as Iterable>::Item>>>;
 
 /// Rule that applies to the initialization of a `Vec`
-pub type InitVecRule<'a, RULE> = InitRule<'a, RULE, Vec<<RULE as Rule>::Item>>;
+pub type InitVecRule<RULE> = InitRule<RULE, Vec<<RULE as Rule>::Item>>;
 
 /// Rule that applies to the initialization of a `VecDeque`
-pub type InitVecDequeRule<'a, RULE> = InitRule<'a, RULE, VecDeque<<RULE as Rule>::Item>>;
+pub type InitVecDequeRule<RULE> = InitRule<RULE, VecDeque<<RULE as Rule>::Item>>;
 
 /// Rule that applies to the initialization of a `String`
-pub type InitStringRule<'a, RULE> = InitRule<'a, RULE, String>;
+pub type InitStringRule<RULE> = InitRule<RULE, String>;
 
 #[cfg(test)]
 mod tests {
