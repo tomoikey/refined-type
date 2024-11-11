730ef32cdbf43e86bfab08b3890aae8eb76d3363
tomoikey <55743826+tomoikey@users.noreply.github.com>
2024-11-12 01:44:49 +0900

    implement Length

--- src/rule/length/equal.rs
+++ src/rule/length/equal.rs
@@ -5,10 +5,53 @@ use crate::Refined;
 /// A type that holds a value satisfying the `LengthEqualRule`
 pub type LengthEqual<const LENGTH: usize, ITEM> = Refined<LengthEqualRule<LENGTH, ITEM>>;
 
+/// A type that holds a value satisfying the `LengthEqualVecRule`
+pub type LengthEqualVec<const LENGTH: usize, ITEM> = Refined<LengthEqualVecRule<LENGTH, ITEM>>;
+
+/// A type that holds a value satisfying the `LengthEqualVecDequeRule`
+pub type LengthEqualVecDeque<const LENGTH: usize, ITEM> =
+    Refined<LengthEqualVecDequeRule<LENGTH, ITEM>>;
+
+/// A type that holds a value satisfying the `LengthEqualHashMapRule`
+pub type LengthEqualHashMap<const LENGTH: usize, K, V> =
+    Refined<LengthEqualHashMapRule<LENGTH, K, V>>;
+
+/// A type that holds a value satisfying the `LengthEqualHashSetRule`
+pub type LengthEqualHashSet<const LENGTH: usize, ITEM> =
+    Refined<LengthEqualHashSetRule<LENGTH, ITEM>>;
+
+/// A type that holds a value satisfying the `LengthEqualStringRule`
+pub type LengthEqualString<const LENGTH: usize> = LengthEqual<LENGTH, String>;
+
+/// A type that holds a value satisfying the `LengthEqualStrRule`
+pub type LengthEqualStr<'a, const LENGTH: usize> = LengthEqual<LENGTH, &'a str>;
+
 /// Rule where the input `ITEM` has a length equal to `LENGTH`
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd)]
 pub struct LengthEqualRule<const LENGTH: usize, ITEM> {
     _phantom: std::marker::PhantomData<ITEM>,
 }
+
+/// Rule where the input `Vec` has a length equal to `LENGTH`
+pub type LengthEqualVecRule<const LENGTH: usize, T> = LengthEqualRule<LENGTH, Vec<T>>;
+
+/// Rule where the input `VecDeque` has a length equal to `LENGTH`
+pub type LengthEqualVecDequeRule<const LENGTH: usize, T> =
+    LengthEqualRule<LENGTH, std::collections::VecDeque<T>>;
+
+/// Rule where the input `HashMap` has a length equal to `LENGTH`
+pub type LengthEqualHashMapRule<const LENGTH: usize, K, V> =
+    LengthEqualRule<LENGTH, std::collections::HashMap<K, V>>;
+
+/// Rule where the input `HashSet` has a length equal to `LENGTH`
+pub type LengthEqualHashSetRule<const LENGTH: usize, T> =
+    LengthEqualRule<LENGTH, std::collections::HashSet<T>>;
+
+/// Rule where the input `String` has a length equal to `LENGTH`
+pub type LengthEqualStringRule<const LENGTH: usize> = LengthEqualRule<LENGTH, String>;
+
+/// Rule where the input `&str` has a length equal to `LENGTH`
+pub type LengthEqualStrRule<'a, const LENGTH: usize> = LengthEqualRule<LENGTH, &'a str>;
 
 impl<const LENGTH: usize, ITEM: LengthDefinition> Rule for LengthEqualRule<LENGTH, ITEM> {
     type Item = ITEM;
--- src/rule/length/less.rs
+++ src/rule/length/less.rs
@@ -5,10 +5,50 @@ use crate::Refined;
 /// A type that holds a value satisfying the `LengthLessRule`
 pub type LengthLess<const THAN: usize, ITEM> = Refined<LengthLessRule<THAN, ITEM>>;
 
+/// A type that holds a value satisfying the `LengthLessVecRule`
+pub type LengthLessVec<const THAN: usize, ITEM> = Refined<LengthLessVecRule<THAN, ITEM>>;
+
+/// A type that holds a value satisfying the `LengthLessVecDequeRule`
+pub type LengthLessVecDeque<const THAN: usize, ITEM> = Refined<LengthLessVecDequeRule<THAN, ITEM>>;
+
+/// A type that holds a value satisfying the `LengthLessHashMapRule`
+pub type LengthLessHashMap<const THAN: usize, K, V> = Refined<LengthLessHashMapRule<THAN, K, V>>;
+
+/// A type that holds a value satisfying the `LengthLessHashSetRule`
+pub type LengthLessHashSet<const THAN: usize, ITEM> = Refined<LengthLessHashSetRule<THAN, ITEM>>;
+
+/// A type that holds a value satisfying the `LengthLessStringRule`
+pub type LengthLessString<const THAN: usize> = LengthLess<THAN, String>;
+
+/// A type that holds a value satisfying the `LengthLessStrRule`
+pub type LengthLessStr<'a, const THAN: usize> = LengthLess<THAN, &'a str>;
+
 /// Rule where the input `ITEM` has a length less than `THAN`
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd)]
 pub struct LengthLessRule<const THAN: usize, ITEM> {
     _phantom: std::marker::PhantomData<ITEM>,
 }
+
+/// Rule where the input `Vec` has a length less than `THAN`
+pub type LengthLessVecRule<const THAN: usize, ITEM> = LengthLessRule<THAN, Vec<ITEM>>;
+
+/// Rule where the input `VecDeque` has a length less than `THAN`
+pub type LengthLessVecDequeRule<const THAN: usize, ITEM> =
+    LengthLessRule<THAN, std::collections::VecDeque<ITEM>>;
+
+/// Rule where the input `HashMap` has a length less than `THAN`
+pub type LengthLessHashMapRule<const THAN: usize, K, V> =
+    LengthLessRule<THAN, std::collections::HashMap<K, V>>;
+
+/// Rule where the input `HashSet` has a length less than `THAN`
+pub type LengthLessHashSetRule<const THAN: usize, ITEM> =
+    LengthLessRule<THAN, std::collections::HashSet<ITEM>>;
+
+/// Rule where the input `String` has a length less than `THAN`
+pub type LengthLessStringRule<const THAN: usize> = LengthLessRule<THAN, String>;
+
+/// Rule where the input `&str` has a length less than `THAN`
+pub type LengthLessStrRule<'a, const THAN: usize> = LengthLessRule<THAN, &'a str>;
 
 impl<const THAN: usize, ITEM: LengthDefinition> Rule for LengthLessRule<THAN, ITEM> {
     type Item = ITEM;
